11ae21f9b6aff861c5b34e6c919d57ccf83439c6
𝓢 <fireeace@gmail.com>
2025-08-15 22:46:21 +0100

    Add files via upload

---
+++ NomeIdadeCidade.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade:")
+cidade = input("Digite sua cidade:")
+print("Olá",nome,"pelas informações que tenho, você possui",idade,"anos de idade, e mora em",cidade )
