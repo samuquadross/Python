bda846a27bbfc9bda12529cbd7327cbe3be76619
𝓢 <fireeace@gmail.com>
2025-08-19 15:06:57 +0100

    Update and rename Medida.py to 2.0.Medida.py

--- Medida.py
+++
@@ -1,8 +0,0 @@
-n = float(input('Digite uma distancia em metros: '))
-cm = n  * 100
-mm = n * 1000
-dm = n * 10
-dam = n / 10
-hm = n / 100
-km = n / 1000
-print('Centimetros: {}\nMilímetros: {}\nDecímetro: {}\nDecâmetro: {}\nHectômetro: {}\nQuilômetro: {}'.format(cm, mm, dm, dam, hm, km))
