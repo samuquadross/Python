a7793ceb1405f9a77798291064d6f64119837c93
𝓢 <fireeace@gmail.com>
2025-08-19 16:11:03 +0100

    Add files via upload

---
+++ PintandoParede.py
@@ -0,0 +1,8 @@
+l = float(input('largura da parede: '))
+r = float(input('altura da parede: '))
+a = l * r
+t = 2
+r = a / t
+print('='* 12)
+print('A Área da sua parede é: {}m² \nE precisará de {}L de tinta para ser completa'.format(a, r))
+print('='* 12)
