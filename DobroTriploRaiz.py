08b5377d8bb30046446ff56362c2f1935b537c07
𝓢 <fireeace@gmail.com>
2025-08-19 10:24:46 +0100

    Add files via upload

---
+++ DobroTriploRaiz.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um Número:'))
+dobro = n * 2
+triplo = n * 3
+raiz = n ** (1/2)
+print('O *Dobro* de {} é: {} \nO *Triplo* de {} é: {} \nA *Raiz Quadrada* de {} é: {:.1f}.'.format(n, dobro, n, triplo, n, raiz,))
