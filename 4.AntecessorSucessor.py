87738353307be57cddef70143f16fe7a9b74a271
𝓢 <fireeace@gmail.com>
2025-08-19 10:27:29 +0100

    Update and rename AntecessorSucessor.py to 4.AntecessorSucessor.py

---
+++ 4.AntecessorSucessor.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um Número:'))
+a = n - 1
+s = n + 1
+print('O *Antecessor* de {} é: {} \nO *Sucessor* de {} é: {}.'.format(n,a,n,s))
--- AntecessorSucessor.py
+++
@@ -1,4 +0,0 @@
-n = int(input('Digite um Número:'))
-a = n - 1
-s = n + 1
-print('O *Antecessor* de {} é: {} \nO *Sucessor* de {} é: {}.'.format(n,a,n,s))
