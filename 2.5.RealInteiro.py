6a9f08f01bd1e6024619e423bada6ac39e2782be
𝓢 <fireeace@gmail.com>
2025-08-20 13:21:48 +0100

    Update 2.5.RealInteiro.py

--- 2.5.RealInteiro.py
+++ 2.5.RealInteiro.py
@@ -1,6 +1,7 @@
 import math
 n = float(input("Digite um número real qualquer: "))
-r = math.floor(n)
+r = math.trunc(n)
 print('='*40)
 print('O número {} tem a parte {} inteira'.format(n, r))
 print('='*40)
+
