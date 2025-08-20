58d2a45397e8b3fb55f6a13119da505685edd09e
𝓢 <fireeace@gmail.com>
2025-08-20 12:07:46 +0100

    Add files via upload

---
+++ SorteadorLista.py
@@ -0,0 +1,12 @@
+import random
+from random import randint
+
+n1 = input('Primeiro nome: ')
+n2 = input('Segundo nome: ')
+n3 = input('Terceiro nome: ')
+n4 = input('Quarto nome: ')
+p = (n1,n2,n3,n4)
+sorteio = sorted(p)
+print('='*22)
+print('O sorteado foi: {}'.format(sorteio))
+print('='*22)
