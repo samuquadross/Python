b974de8cb5eda194dc1c44cb685917b6c115594f
𝓢 <fireeace@gmail.com>
2025-08-22 16:49:58 +0100

    Update and rename LerString.py to 3.3.LerString.py

---
+++ 3.3.LerString.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite seu nome completo: ').split())
+print('Seu nome tem SILVA? {}'.format("SILVA" in nome.upper()))
--- LerString.py
+++
@@ -1,2 +0,0 @@
-nome = str(input('Digite seu nome completo: ').split())
-print('Seu nome tem SILVA? {}'.format("SILVA" in nome.upper()))
