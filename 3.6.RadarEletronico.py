b9df79531629ecca44c183ae0b9ef2783c87762f
𝓢 <fireeace@gmail.com>
2025-08-25 12:11:30 +0100

    Update 3.6.RadarEletronico.py

--- 3.6.RadarEletronico.py
+++ 3.6.RadarEletronico.py
@@ -7,5 +7,5 @@ if v >= 80:
     print('\U000026A0\U0000FE0F Você ultrapassou o limite de velocidade!\U000026A0\U0000FE0F')
     print('Total a pagar sobre a multa: {:.2f} €'.format(m))
 else:
-    print('Nenhum limite de velocidade foi ultrapassado!')
+    print('\U00002705 Nenhum limite de velocidade foi ultrapassado!\U00002705')
 print('='*34)
