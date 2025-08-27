139458710dd7a64365b8b2130ee5e4764b599486
𝓢 <fireeace@gmail.com>
2025-08-27 10:52:20 +0100

    Update 4.1.AnalisadorTriangulo.py

--- 4.1.AnalisadorTriangulo.py
+++ 4.1.AnalisadorTriangulo.py
@@ -1,8 +1,10 @@
-print('============ ANALISADOR DE TRIANGULO ===========')
+print('================= ANALISADOR DE TRIANGULO =================')
 n = float(input('Primeiro segmento: '))
 m = float(input('Segundo segmento: '))
 p = float(input('Terceiro segmento: '))
-print('================================================')
+print('===========================================================')
 
 if n < m + p and m < n + p and p < n + m:
-    print("\U00002705 Os segmentos acima podem formar um triangulo \U00002705")
+    print("\U00002705 \033[32mOs segmentos acima podem formar um triangulo\033[m \U00002705")
+else:
+    print("\U0000274C \033[31mOs segmentos inseridos não podem formar um triangulo\033[m \U0000274C")
