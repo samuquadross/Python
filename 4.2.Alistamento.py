400ff0e21b0b92821fb5cfaaa531a06c8edd3cb8
𝓢 <fireeace@gmail.com>
2025-08-28 15:35:16 +0100

    Update 4.2.Alistamento.py

--- 4.2.Alistamento.py
+++ 4.2.Alistamento.py
@@ -1,15 +1,16 @@
 a = int(input('Ano de nascimento: '))
 r = (2025-a)
-print("\033[34m="*45)
-print('\033[34mVocê poussui {} anos de idade'.format(r))
-print('\033[34m='*45)
+
+if a > 2025 or a < 1875:
+    print('Introduza um ano válido!')
+    exit()
+else:
+    print('Você possui {} anos de idade'.format(r))
 if r == 18:
-    print('\033[32m=============================================\n'
-          '\033[32mVocê já pode se alistar no exército!')
+    print('\033[32mVocê já pode se alistar no exército!')
 elif r < 18:
-    print('\033[33m=============================================\n'
-          '\033[33mVocê ainda não pode se alistar no exército!')
+    print('\033[33mVocê ainda não pode se alistar no exército!')
+    print('\033[33mFaltam {} anos'.format(18-r))
 elif r > 18:
-    print('\033[31m=============================================\n'
-          '\033[31mVocê já passou do tempo de se alistar!')
-print("="*45)
+    print('\033[31mVocê já passou do tempo de se alistar!')
+    
