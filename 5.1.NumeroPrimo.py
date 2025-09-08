1f2500dd31c8bfbb0ccb99caa9fa5c0c37f58301
𝓢 <fireeace@gmail.com>
2025-09-08 16:47:37 +0100

    Update 5.1.NumeroPrimo.py

--- 5.1.NumeroPrimo.py
+++ 5.1.NumeroPrimo.py
@@ -1,4 +1,4 @@
-print('\033[1;33m========== Número Primo ==========\033[0m')
+print('========== Número Primo ==========')
 num = int(input("Digite um número: "))
 r = 0
 for i in range(1, num+1):
@@ -8,6 +8,9 @@ for i in range(1, num+1):
     else:
         print("\033[31m", end='')
     print("{} ".format(i), end="")
-print('\n\033[1;33m==================================\033[0m')
-print('\033[1;34mO numero \033[4m{}\033[0m \033[1;34mfoi divisível \033[4m{}\033[0m \033[1;34mvezes'.format(num, r))
-
+print('\n\033[0m==================================')
+print('O numero \033[4;34m{}\033[0m foi divisível \033[4;34m{}\033[0m vezes'.format(num, r))
+if r == 2:
+    print('\033[32mE por isso ele é PRIMO!\033[0m')
+else:
+    print('\033[31mE por isso ele NÃO É PRIMO!\033[0m')
